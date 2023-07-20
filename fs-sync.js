af12b12a1fa277b0637935b00b8bc116720511b6
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-07-20 23:49:43 +0530

    intro to http

--- fs-sync.js
+++ fs-sync.js
@@ -1,18 +1,20 @@
 //File system in sync mode
 const {writeFileSync, readFileSync} = require('fs')
-
+console.log("start");
 const first = readFileSync('./content/first.txt','utf8')
 const second = readFileSync('./content/second.txt','utf8')
 
 // console.log(first,second)
 //for writing in the file
-writeFileSync(
-'./content/result-sync.txt',
-`Here is the result: ${first}, ${second}`
-)
+// writeFileSync(
+// './content/result-sync.txt',
+// `Here is the result: ${first}, ${second}`
+// )
 //for appending into the file
 writeFileSync(
     './content/result-sync.txt',
     `Here is the result: ${first}, ${second}`,
     {flag : 'a'}
     )
+    console.log("Done with the task");
+    console.log("starting the next one")
---
+++ http.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/"){
+    res.end("Welcome to node js page")
+  }
+  if(req.url === '/about'){
+    res.end("developed by Gaurav Prajapati");
+  }
+
+  res.end(
+    `<h1>Oops!</h1>
+    <p>Page does not exist here please add a corrent URL</p>
+    <a href="/" >Go to Home</a>`
+  )
+  
+})
+
+server.listen(5000);
